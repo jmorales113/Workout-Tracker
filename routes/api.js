345476f757edf1e1be60f0a5efeb9324b9f86df7
jmorales113 <jmorales113@gmail.com>
2020-04-27 16:59:45 -0700

    Added get and post route

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,25 @@ const db = require('../models')
 
 
 
+function apiRoutes(app) {
+    app.get("/api/workouts", (req, res) => {
+        db.Workout.find({})
+        .then((result) =>{
+            res.json(result)
+        }).catch((error) => {
+            res.json(error)
+        })
+    })
 
+    app.post("/api/workouts", (req, res) =>{
+        db.Workout.create({})
+        .then((result) =>{
+            res.json(result)
+        }).catch((error) =>{
+            res.json(error)
+        })
+    })
+}
 
 
 
