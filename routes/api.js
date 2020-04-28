6b5e96f6c8b344737df5f904df278c78b3de902a
jmorales113 <jmorales113@gmail.com>
2020-04-27 19:28:42 -0700

    Added put route

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose')
 const db = require('../models')
 
-
+// db.Workout.find({})
+// db.Workout.create({})
+// db.Workout.findByIdAndUpdate
+// db.Workout.find({}).limit(7)
 
 function apiRoutes(app) {
     app.get("/api/workouts", (req, res) => {
@@ -16,6 +19,24 @@ function apiRoutes(app) {
     app.post("/api/workouts", (req, res) =>{
         db.Workout.create({})
         .then((result) =>{
+            res.json(result)
+        }).catch((error) =>{
+            res.json(error)
+        })
+    })
+
+    app.put("/api/workouts/:id", (req, res) => {
+        db.Workout.findByIdAndUpdate(
+            {
+                _id: req.params.id
+            },
+            {$push: {exercises: req.body}},
+            {
+                new: true
+            }
+            
+        )
+        .then((result) => {
             res.json(result)
         }).catch((error) =>{
             res.json(error)
